4a695fffc219d26fa3c3ba710c11ef76ba2b4e44
kevinbpatel <kevnpatel99@gmail.com>
2025-07-04 14:10:15 -0400

    Add ESlint and prettier dev dependencies

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This repository contains the basic webpack template as done in the Odin Project.
 
 Below are the specific links to the resources followed:
+
 - [JavaScript Course: Webpack](https://www.theodinproject.com/lessons/javascript-webpack)
 - [JavaScript Course: Revisiting Webpack](https://www.theodinproject.com/lessons/node-path-javascript-revisiting-webpack)
 - [Webpack’s “Production” guide](https://webpack.js.org/guides/production/)
+- [JavaScript Course: Linting](https://www.theodinproject.com/lessons/node-path-javascript-linting)
--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Production',
-      template: './src/template.html',
+      title: "Production",
+      template: "./src/template.html",
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
